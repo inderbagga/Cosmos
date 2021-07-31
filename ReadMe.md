d2a9fb87bd7527f2e04219f21c86aaaeb098c1cf
Inder Bagga <er.inderbagga@gmail.com>
2021-07-31 10:13:13 +0530

    Adding Databinding.

--- ReadMe.md
+++ ReadMe.md
@@ -3,5 +3,6 @@
 1. create data class
 2. add httpclient and api service
 3. add activity xml
+3. add databinding
 
 
--- app/src/main/java/com/inderbagga/cosmos/MainActivity.kt
+++ app/src/main/java/com/inderbagga/cosmos/MainActivity.kt
@@ -2,10 +2,18 @@ package com.inderbagga.cosmos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.inderbagga.cosmos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding= DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner=this
     }
 }
